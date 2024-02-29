43f11fd925e814ce76c48a7c86fe957368e9119c
Kostia <kostia3507@gmail.com>
2024-02-29 12:39:12 +0200

    update get one product method in product service

--- src/products/services.py
+++ src/products/services.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from typing_extensions import Dict, Optional
 
 from src.repositories.json_repository import JSONRepository
@@ -5,16 +6,21 @@ from src.repositories.json_repository import JSONRepository
 
 class ProductService:
     def __init__(self, product_repo: JSONRepository) -> None:
-        self.product_repo: JSONRepository = product_repo
+        self.product_repo: JSONRepository = product_repo("D:\\py_projects\\fastapi-mcdonalds-scrapping\\mcdonalds_menu.json")
 
     async def get_product(self, product_name: str) -> Optional[Dict[str, str]]:
-        return await self.product_repo.get_one(product_name)
+        product = await self.product_repo.get_one(product_name)
+
+        if product:
+            return product
+
+        raise HTTPException(status_code=404, detail="Product with this name does not exist")
 
     async def get_all_products(self) -> dict[str, dict]:
         return await self.product_repo.get_all()
 
     async def get_product_field(self, product_name: str, product_field: str) -> Optional[str]:
         product = await self.get_product(product_name)
-        if product is not None:
-            return product.get(product_field)
-        return None
+        if product:
+            return {product_field: product.get(product_field)}
+        raise HTTPException(status_code=404, detail="No such field in this product")
