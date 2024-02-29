8e6268fdd78365ad1b6f542b71abc84be5522e55
Kostia <kostia3507@gmail.com>
2024-02-29 12:42:02 +0200

    added get product field endpoint

--- src/products/routers.py
+++ src/products/routers.py
@@ -16,3 +16,8 @@ async def get_all_products(product_service=Depends(get_product_service)):
 @products_router.get("/products/{product_name}")
 async def get_product_by_name(product_name: str, product_service=Depends(get_product_service)):
     return await product_service.get_product(product_name)
+
+
+@products_router.get("/products/{product_name}/{product_field}")
+async def get_product_field(product_name: str, product_field: str, product_service=Depends(get_product_service)):
+    return await product_service.get_product_field(product_name, product_field)
