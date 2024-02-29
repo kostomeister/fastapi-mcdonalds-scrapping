36e378114e6c72e0c15f61db74a4469a292be1e1
Kostia <kostia3507@gmail.com>
2024-02-29 12:41:47 +0200

    added get product by name endpoint

--- src/products/routers.py
+++ src/products/routers.py
@@ -11,3 +11,8 @@ products_router = APIRouter()
 @products_router.get("/all_products/")
 async def get_all_products(product_service=Depends(get_product_service)):
     return await product_service.get_all_products()
+
+
+@products_router.get("/products/{product_name}")
+async def get_product_by_name(product_name: str, product_service=Depends(get_product_service)):
+    return await product_service.get_product(product_name)
